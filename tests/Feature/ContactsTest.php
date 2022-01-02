238468df26fc3934e8587633640449fae14eb477
nishimura00t <t.nishimura@scene-live.com>
2022-01-02 17:35:32 +0900

    [update]各種項目テスト

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -14,9 +14,47 @@ class ContactsTest extends TestCase
     /** @test */
     public function a_contact_can_be_added()
     {
+        // 例外処理を省く
         $this->withoutExceptionHandling();
-        $this->post('/api/contacts', ['name' => 'Test Name']);
+        $this->post('/api/contacts', [
+            'name' => 'Test Name',
+            'email' => 'test@gmail.con',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String'
+        ]);
 
-        $this->assertCount(1, Contact::all());
+        $contact = Contact::first();
+
+        $this->assertEquals('Test Name', $contact->name);
+        $this->assertEquals('test@gmail.con', $contact->email);
+        $this->assertEquals('05/14/1988', $contact->birthday);
+        $this->assertEquals('ABC String', $contact->company);
     }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/api/contacts', [
+            'email' => 'test@gmail.con',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String'
+        ]);
+
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /** @test */
+    public function a_email_is_required()
+    {
+        $response = $this->post('/api/contacts', [
+            'name' => 'Test Name',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String'
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
+    }
+
 }
