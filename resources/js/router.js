32c738f4e425dbe5777c625c9c5d73f70b80def8
tomoyuki <xxbgxxlk.tm@gmail.com>
2022-01-25 23:21:03 +0900

    [update]add_contact_show

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ExampleComponent from './components/ExampleComponent';
 import ContactCreate from './views/ContactCreate';
+import ContactsShow from './views/ContactsShow';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     routes: [
         { path: '/', component: ExampleComponent },
-        { path: '/contacts/create', component: ContactCreate }
+        { path: '/contacts/create', component: ContactCreate },
+        { path: '/contacts/:id', component: ContactsShow },
     ],
     mode: 'history',
 })
---
+++ resources/js/views/ContactsShow.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="flex justify-between">
+            <div class="text-blue-400">
+                < Back
+            </div>
+            <div>
+                <router-link :to="'/contact/' + contact.id + '/edit'"
+                    class="px-4 py-2 rounded text-sm text-green-500 border border-green-500 font-bold mr-2"
+                >Edit</router-link>
+                <a href="#"
+                    class="px-4 py-2 text-red-500 border border-red-500 rounded text-sm font-bold"
+                >Delete</a>
+            </div>
+        </div>
+
+        <div class="flex items-center pt-6">
+            <div>VG</div>
+
+            <p class="pl-5 text-xl">{{ contact.name }}</p>
+        </div>
+
+        <p class="pt-6 text-gray-600 font-bold text-sm">Contact</p>
+        <p class="pt-2 text-blue-400">{{ contact.email }}</p>
+
+        <p class="pt-6 text-gray-600 font-bold text-sm">Company</p>
+        <p class="pt-2 text-blue-400">{{ contact.company }}</p>
+
+        <p class="pt-6 text-gray-600 font-bold text-sm">Birthday</p>
+        <p class="pt-2 text-blue-400">{{ contact.birthday }}</p>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ContactsShow",
+
+    mounted() {
+        axios.get('/api/contacts/' + this.$route.params.id)
+            .then(response => {
+                this.contact = response.data.data;
+            })
+            .catch(error => {
+
+            });
+    },
+
+    data: function () {
+        return {
+            contact: null,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
