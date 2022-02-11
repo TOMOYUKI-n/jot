8065fc7a5037071b3046c10ff8640d1a942a3e99
tomoyuki <xxbgxxlk.tm@gmail.com>
2022-02-11 22:03:27 +0900

    [add]誕生月が近い人を表示するコンポーネント作成

---
+++ resources/js/views/BirthdaysIndex.vue
@@ -0,0 +1,13 @@
+<template>
+    <ContactsList endpoint="/api/birthdays" />
+</template>
+<script>
+import ContactsList from '../components/ContactsList.vue';
+export default {
+    name: "ContactsIndex",
+
+    components: {
+        ContactsList
+    }
+}
+</script>
