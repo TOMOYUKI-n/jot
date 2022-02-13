34d139a7887abef27886559d0e3c5f95ad99ecc3
tomoyuki <xxbgxxlk.tm@gmail.com>
2022-02-13 15:56:01 +0900

    [update]sourcemapを読み込むようにした

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 require('mix-tailwindcss');
 
-mix.js('resources/js/app.js', 'public/js').vue()
+mix.js('resources/js/app.js', 'public/js').vue().sourceMaps()
     .sass('resources/sass/app.scss', 'public/css').tailwind();
