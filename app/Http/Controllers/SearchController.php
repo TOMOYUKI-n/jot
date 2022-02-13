24e6a950bc77867ef960cddb697b8b0ae73445db
tomoyuki <xxbgxxlk.tm@gmail.com>
2022-02-13 15:07:08 +0900

    [update]自分が登録したコンタクトから検索をかけるようにする

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\Contact as ResourcesContact;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SearchController extends Controller
 {
@@ -14,7 +15,9 @@ class SearchController extends Controller
             'searchTerm' => 'required',
         ]);
 
-        $contacts = Contact::search($data['searchTerm'])->get();
+        $contacts = Contact::search($data['searchTerm'])
+            ->where('user_id', request()->user()->id)    
+            ->get();
 
         return ResourcesContact::collection($contacts);
     }
